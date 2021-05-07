e65e810916c77c7b848eacdb790a98e4cd89cf84
Ankita Masalkhamb <ankita.masalkhamb@iping.in>
2021-05-07 22:10:14 +0530

    files added

---
+++ database/migrations/2021_05_03_191453_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('cat_id');
+            $table->string('cat_name')->unique()->nullable();
+            $table->string('cat_description')->nullable();
+            $table->string('cat_image')->nullable();
+            $table->boolean('deleted')->default(0);
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
