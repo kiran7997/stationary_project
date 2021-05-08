af918baf68def1f2ae6d4493a9865d44b16bafab
shilpajdhv4 <shilpa.jadhav@iping.in>
2021-05-08 17:34:08 +0530

    User and Department pages intergration done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,52 +12,70 @@ License: You must have a valid license purchased only from themeforest(the above
 
 -->
 <html class="loading semi-dark-layout" lang="en" data-layout="semi-dark-layout" data-textdirection="ltr">
-  <!-- BEGIN: Head-->
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
-    <title>@yield('title')</title>
-    <link rel="apple-touch-icon" href="{{ asset('app-assets/images/ico/apple-icon-120.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/ico/favicon.ico') }}">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+<!-- BEGIN: Head-->
 
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/charts/apexcharts.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/extensions/toastr.min.css') }}">
-    <!-- END: Vendor CSS-->
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+  <meta name="description"
+    content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+  <meta name="keywords"
+    content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+  <meta name="author" content="PIXINVENT">
+  <title>@yield('title')</title>
+  <link rel="apple-touch-icon" href="{{ asset('app-assets/images/ico/apple-icon-120.png') }}">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/ico/favicon.ico') }}">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+    rel="stylesheet">
 
-    <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/bordered-layout.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.min.css') }}">
+  <!-- BEGIN: Vendor CSS-->
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/charts/apexcharts.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/extensions/toastr.min.css') }}">
+  <!-- END: Vendor CSS-->
 
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/vertical-menu.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/pages/dashboard-ecommerce.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/plugins/charts/chart-apex.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/plugins/extensions/ext-component-toastr.min.css') }}">
-    <!-- END: Page CSS-->
+  <!-- BEGIN: Theme CSS-->
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/bordered-layout.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.min.css') }}">
 
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-    <!-- END: Custom CSS-->
+  <!-- BEGIN: Page CSS-->
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/css/core/menu/menu-types/vertical-menu.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/pages/dashboard-ecommerce.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/plugins/charts/chart-apex.min.css') }}">
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/css/plugins/extensions/ext-component-toastr.min.css') }}">
+  <!-- END: Page CSS-->
 
-  </head>
+  <!-- BEGIN: Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+  <!-- END: Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/forms/select/select2.min.css') }}" />
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css') }}" />
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/css/plugins/forms/pickers/form-flat-pickr.min.css') }}" />
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/vendors/css/tables/datatable/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/vendors/css/tables/datatable/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
 
-  <body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
-      @include('layouts/common/header')
-      @include('layouts/common/sidebar')
-      @yield('content')
-      @include('layouts/common/footer')
-  </body>
-  <!-- END: Body-->
+</head>
+
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
+  data-menu="vertical-menu-modern" data-col="">
+  @include('layouts/common/header')
+  @include('layouts/common/sidebar')
+  @yield('content')
+  @include('layouts/common/footer')
+</body>
+<!-- END: Body-->
+
 </html>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,41 +1,118 @@
 @extends('layouts.app')
-
-
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2> Show User</h2>
+<!-- Responsive Datatable -->
+<!-- BEGIN: Content-->
+<div class="app-content content ">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-9 col-12 mb-2">
+                <div class="row breadcrumbs-top">
+                    <div class="col-12">
+                        <h2 class="content-header-title float-left mb-0">Users Management</h2>
+                        <div class="breadcrumb-wrapper">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a>
+                                </li>
+                                <li class="breadcrumb-item"><a href="{{route('users.index')}}">Users</a>
+                                </li>
+                                <li class="breadcrumb-item active">User
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
+        <div class="content-body">
+            <section id="responsive-datatable">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header border-bottom">
+                                <h4 class="card-title">Users Details</h4>
+                            </div>
+                            <div style="margin:20px;">
+                                @if ($message = Session::get('success'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    <div class="alert-body">
+                                        <p>{{ $message }}</p>
+                                    </div>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                @endif
+                                <!-- User Card starts-->
+                                <div class="col-xl-9 col-lg-8 col-md-7">
+                                    <div class="card user-card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div
+                                                    class="col-xl-6 col-lg-12 d-flex flex-column justify-content-between border-container-lg">
+                                                    <div class="user-avatar-section">
+                                                        <div class="d-flex justify-content-start">
+                                                            <img class="img-fluid rounded"
+                                                                src="{{url('user_images/'.$user->profile_image)}}"
+                                                                height="104" width="104" alt="User avatar" />
+                                                            <div class="d-flex flex-column ml-1">
+                                                                <div class="user-info mb-1">
+                                                                    <h4 class="mb-0">{{ $user->firstname }}</h4>
+                                                                    <span class="card-text">{{ $user->email }}</span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                <div class="col-xl-6 col-lg-12 mt-2 mt-xl-0">
+                                                    <div class="user-info-wrapper">
+                                                        <div class="d-flex flex-wrap">
+                                                            <div class="user-info-title">
+                                                                <i data-feather="user" class="mr-1"></i>
+                                                                <span
+                                                                    class="card-text user-info-title font-weight-bold mb-0">Username</span>
+                                                            </div>
+                                                            <p class="card-text mb-0">&nbsp;{{ $user->username }}</p>
+                                                        </div>
+
+                                                        <div class="d-flex flex-wrap my-50">
+                                                            <div class="user-info-title">
+                                                                <i data-feather="star" class="mr-1"></i>
+                                                                <span
+                                                                    class="card-text user-info-title font-weight-bold mb-0">Department</span>
+                                                            </div>
+                                                            <p class="card-text mb-0"> &nbsp;
+                                                                @if(!empty($user->getRoleNames()))
+                                                                @foreach($user->getRoleNames() as $v)
+                                                                <label class="badge badge-success">{{ $v }}</label>
+                                                                @endforeach
+                                                                @endif</p>
+                                                        </div>
+
+                                                        <div class="d-flex flex-wrap">
+                                                            <div class="user-info-title">
+                                                                <i data-feather="phone" class="mr-1"></i>
+                                                                <span
+                                                                    class="card-text user-info-title font-weight-bold mb-0">Contact</span>
+                                                            </div>
+                                                            <p class="card-text mb-0">&nbsp; {{ $user->phone_no }}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /User Card Ends-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 </div>
 
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-            {{ $user->name }}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Email:</strong>
-            {{ $user->email }}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Roles:</strong>
-            @if(!empty($user->getRoleNames()))
-            @foreach($user->getRoleNames() as $v)
-            <label class="badge badge-success">{{ $v }}</label>
-            @endforeach
-            @endif
-        </div>
-    </div>
-</div>
 @endsection
