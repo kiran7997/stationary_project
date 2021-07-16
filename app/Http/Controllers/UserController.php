08fcaabaf1b8f66d91673bd217db2be35daace78
Rajlaxmi Kairamkonda <rajlaxmi.kairamkonda@iping.in>
2021-07-16 18:18:43 +0530

    po listing changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -369,7 +369,7 @@ class UserController extends Controller
         $order_list = DB::table('notifications')
                     ->select('notifications.order_id','orders.*')
                     ->leftjoin('orders','orders.order_id','notifications.order_id')
-                    ->where(['notifications.user_id'=>$user_id,'is_read'=>0,'order_status' => 'payment_completed'])
+                    ->where(['notifications.user_id'=>$user_id,'order_status' => 'payment_completed'])
                     ->get();
         return view('employee-dashboard-list.sales-order-list',compact('order_list'));   
     }
